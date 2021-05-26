83ac16d9e9aef4c4572e31ccbbec9d7123dc45cf
Troy <tvinsixiengmay@gmail.com>
2021-05-26 16:58:57 -0700

    Clarified test print messages

--- main.cpp
+++ main.cpp
@@ -5,18 +5,24 @@
 int main()
 {
     std::string testString("cabbage");
+    std::cout << "Test String: " << testString << std::endl;
 
     Skew s;
+    std::cout << typeid(s).name() << std::endl;
     s.addString(testString);
     s.makeSuffixArray();
     s.printSuffixArray();
+    std::cout << std::endl;
 
     SAIS s2;
+    std::cout << typeid(s2).name() << std::endl;
     s2.addString(testString);
     s2.makeSuffixArray();
     s2.printSuffixArray();
+    std::cout << std::endl;
 
     NaiveSA s3;
+    std::cout << typeid(s3).name() << std::endl;
     s3.addString(testString);
     s3.makeSuffixArray();
     s3.printSuffixArray();
